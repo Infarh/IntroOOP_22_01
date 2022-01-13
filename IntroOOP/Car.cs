51ef112254027d81ae223c3fb836c998bbe908a8
Infarh <shmachilin@yandex.ru>
2022-01-13 20:47:07 +0300

    Развитие идей управления машиной

---
+++ IntroOOP/Car.cs
@@ -0,0 +1,85 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace IntroOOP
+{
+    public class Car
+    {
+        //public event EventHandler Crash;
+        public event Action Crash;
+
+        private string _Name;
+
+        protected double _Position;
+
+        //public double _Speed;
+        protected double _Speed;
+
+        public double Speed
+        {
+            /*private */get { return _Speed; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException(nameof(value), value, "Значение скорости не может быть меньше нуля!");
+
+                _Speed = value;
+                //Console.WriteLine("Новая скорость {0}", value);
+            }
+        }
+
+        public double Position { get => _Position; set => _Position = value; }
+
+        //public double get_Position()
+        //{
+        //    return _Position;
+        //}
+
+        //public void set_Position(double value)
+        //{
+        //    _Position = value;
+        //}
+
+        public double TestPosition { get; set; }
+
+        public string Name => _Name;
+
+        public double TresholdSpeed { get; set; } = 180 / 3.6;
+
+        public Car(string Name, double Position, double Speed = 0)
+        {
+            //Console.WriteLine($"Значение {nameof(Name)} = {Name}");
+
+            _Name = Name;
+            _Position = Position;
+            _Speed = Speed;
+
+            var last_position_time = Position / Speed;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining /*| MethodImplOptions.Synchronized*/)]
+        private static double Power2(double x) => x * x;
+
+        public double Move(double dt, double Acceleration = 0)
+        {
+            //_Position = _Speed * dt + Acceleration * (dt * dt) / 2; //Math.Pow(dt, 2);
+            _Position += _Speed * dt + Acceleration * Power2(dt) / 2;
+            _Speed += Acceleration * dt;
+
+            if (TresholdSpeed > 0 && _Speed >= TresholdSpeed)
+            {
+                Crash?.Invoke();
+            }
+
+            return _Position;
+        }
+    }
+
+    //public class FastCar : Car
+    //{
+    //    public FastCar() : base("Fast", 0, 1000)
+    //    {
+    //        _Position = 50;
+    //        _Speed = 10;
+    //    }
+    //}
+}
--- IntroOOP/Program.cs
+++ IntroOOP/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-using System.Runtime.CompilerServices;
 
 namespace IntroOOP
 {
@@ -10,6 +9,18 @@ namespace IntroOOP
         public static void Main(string[] args)
         {
             var car = new Car("ВАЗ-2141", Position: 140, Speed: 0);
+            //car.TresholdSpeed = double.NaN;
+
+            car.Crash += () =>
+            {
+                //throw new Exception($"Автокатастрофа на скорости {car.Speed * 3.6} км/ч");
+                Console.WriteLine("Опасная скорость! {0} км/ч", car.Speed * 3.6);
+            };
+
+            car.Crash += () =>
+            {
+                throw new Exception($"Автокатастрофа на скорости {car.Speed * 3.6} км/ч");
+            };
 
             const double dt = 0.1;
             //var t = 0d;
@@ -18,72 +29,13 @@ namespace IntroOOP
             {
                 car.Move(dt, 1.5);
                 t += dt;
+
+                //if (car.Speed > 180 / 3.6)
+                //    throw new Exception($"Автокатастрофа на скорости {car.Speed * 3.6} км/ч");
             }
 
             Console.WriteLine("Время разгона составило {0} c, скорость составила {1} км/ч", 
                 t, car.Speed * 3.6);
-        }
-    }
-
-    public class Car
-    {
-        private string _Name;
-
-        protected double _Position;
-
-        //public double _Speed;
-        protected double _Speed;
-
-        public double Speed
-        {
-            /*private */get { return _Speed; }
-            set
-            {
-                if (value < 0)
-                    throw new ArgumentOutOfRangeException(nameof(value), value, "Значение скорости не может быть меньше нуля!");
-
-                _Speed = value;
-                //Console.WriteLine("Новая скорость {0}", value);
-            }
-        }
-
-        public double Position { get => _Position; set => _Position = value; }
-
-        //public double get_Position()
-        //{
-        //    return _Position;
-        //}
-
-        //public void set_Position(double value)
-        //{
-        //    _Position = value;
-        //}
-
-        public double TestPosition { get; set; }
-
-        public string Name => _Name;
-
-        public Car(string Name, double Position, double Speed = 0)
-        {
-            //Console.WriteLine($"Значение {nameof(Name)} = {Name}");
-
-            _Name = Name;
-            _Position = Position;
-            _Speed = Speed;
-
-            var last_position_time = Position / Speed;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining /*| MethodImplOptions.Synchronized*/)]
-        private static double Power2(double x) => x * x;
-
-        public double Move(double dt, double Acceleration = 0)
-        {
-            //_Position = _Speed * dt + Acceleration * (dt * dt) / 2; //Math.Pow(dt, 2);
-            _Position += _Speed * dt + Acceleration * Power2(dt) / 2;
-            _Speed += Acceleration * dt;
-
-            return _Position;
         }
     }
 
