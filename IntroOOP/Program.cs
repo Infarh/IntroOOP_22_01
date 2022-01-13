125ccbdefda8ddc779cd98260d0b2183c347e181
Infarh <shmachilin@yandex.ru>
2022-01-13 20:00:13 +0300

    Эксперименты со свойствами

--- IntroOOP/Program.cs
+++ IntroOOP/Program.cs
@@ -1,13 +1,58 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace IntroOOP
 {
     /// <summary>Класс главной программы</summary>
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
+            var car = new Car("ВАЗ-2141", 140, 200);
+
+            //car._Speed = 50;
+            //car._Position = 0;
+
+            var speed = car.Speed;
+            speed = 300;
+            car.Speed += 50;
+            car.Speed = 150;
+            
+
             Console.WriteLine("213");
+        }
+    }
+
+    public class Car
+    {
+        private string _Name;
+
+        protected double _Position;
+
+        //public double _Speed;
+        private double _Speed;
+
+        public double Speed
+        {
+            get
+            {
+                return _Speed;
+                //return Random.Shared.Next(1, 200);
+            }
+            set
+            {
+                _Speed = value;
+                //Console.WriteLine("Новая скорость {0}", value);
+            }
+        }
+
+        public Car(string Name, double Position, double Speed = 0)
+        {
+            _Name = Name;
+            _Position = Position;
+            _Speed = Speed;
+
+            var last_position_time = Position / Speed;
         }
     }
 }
