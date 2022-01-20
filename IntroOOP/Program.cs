8f93a0f347ba50099847d07e9e4bdd24bc67a6f7
Infarh <shmachilin@yandex.ru>
2022-01-20 20:32:35 +0300

    Двусвязный список - основная логика

--- IntroOOP/Program.cs
+++ IntroOOP/Program.cs
@@ -1,9 +1,44 @@
-﻿namespace IntroOOP;
+﻿using ToolsLib;
+
+
+namespace IntroOOP;
 
 public static class Program
 {
     public static void Main(string[] args)
     {
+        RefList<string> strs_list = new RefList<string>();
+        RefList<string> strs_list2 = new RefList<string>();
+
+        var another_node = strs_list2.AddFirst("000");
+
+        //var strs_node = new RefList<string>.Node();
+
+        for (var i = 1; i < 10; i++)
+        {
+            strs_list.AddLast($"Value-{i}");
+        }
+
+        var position = strs_list.First.Next.Next;
+
+        var node_123 = strs_list.AddAfter(position, "123");
+
+        //strs_list.AddAfter(another_node, "321");
+
+
+        var value_123 = strs_list.Remove(node_123);
+        //strs_list2.AddLast(strs_list.Remove(node_123));
+
+
+        strs_list.Clear();
+
+
+        strs_list.AddLast("111");
+        strs_list.AddLast("222");
+        strs_list.AddLast("33");
+        strs_list.AddLast("444");
+
+        //strs_list.AddAfter(position, "555");
 
         Console.ReadLine();
     }
--- ToolsLib/RefList.cs
+++ ToolsLib/RefList.cs
@@ -8,8 +8,226 @@ namespace ToolsLib;
 
 public class RefList<T>
 {
+    public class Node
+    {
+        internal RefList<T>? _List;
+
+        public T Value { get; set; }
+
+        public Node? Next { get; set; }
+
+        public Node? Prev { get; set; }
+
+        internal Node(T value, RefList<T> List)
+        {
+            Value = value;
+            _List = List;
+        }
+    }
+
+    //public static async Task<RefList<T>> CreateAsync()
+    //{
+    //    await Task.Delay(100);
+
+    //    var list = new RefList<T>();
+    //    return list;
+    //}
+
+    //public static RefList<T> Create()
+    //{
+    //    var list = new RefList<T>();
+    //    return list;
+    //}
+
+    private static RefList<T> __List;
+    private static readonly object __SyncRoot = new();
+
+    //public static RefList<T> List
+    //{
+    //    get
+    //    {
+    //        if (__List != null) return __List;
+    //        __List = new();
+    //        return __List;
+    //    }
+    //}
+
+    public static RefList<T> List
+    {
+        get
+        {
+            if (__List != null) return __List;
+
+            lock (__SyncRoot)
+            {
+                if (__List != null) return __List;
+                __List = new();
+                return __List;
+            }
+        }
+    }
+
+    private int _Count;
+
+    public int Count => _Count;
+
+    //public int Count { get; private set; }
+
+    public Node? First { get; private set; }
+
+    public Node? Last { get; set; }
+
     public RefList()
     {
-        
+
+    }
+
+    public Node AddFirst(T value)
+    {
+        var node = new Node(value, this);
+        _Count++;
+
+        if (First is null)
+        {
+            First = node;
+            Last = node;
+            return node;
+        }
+
+        node.Next = First;
+        First.Prev = node;
+        First = node;
+
+        return node;
+    }
+
+    public Node AddLast(T value)
+    {
+        var node = new Node(value, this);
+        _Count++;
+
+        if (Last is null)
+        {
+            First = node;
+            Last = node;
+            return node;
+        }
+
+        node.Prev = Last;
+        Last.Next = node;
+        Last = node;
+
+        return node;
+    }
+
+    private bool CheckNode(Node node)
+    {
+        var n = First;
+        while (n != null)
+        {
+            if (ReferenceEquals(n, node)) return true;
+            n = n.Next;
+        }
+
+        return false;
+    }
+
+    public Node AddAfter(Node Position, T value)
+    {
+        if (!ReferenceEquals(this, Position._List))
+            throw new InvalidOperationException("Попытка добавления значения после узла, не принадлежащего текущему списку");
+
+        //if (!CheckNode(Position))
+        //    throw new InvalidOperationException("Попытка добавления значения после узла, не принадлежащего текущему списку");
+
+        if (ReferenceEquals(Position, Last))
+            return AddLast(value);
+
+        var node = new Node(value, this)
+        {
+            Prev = Position,
+            Next = Position.Next
+        };
+
+        Position.Next = node;
+        node.Next!.Prev = node;
+
+        return node;
+    }
+
+    public Node AddBefore(Node Position, T value)
+    {
+        if (!ReferenceEquals(this, Position._List))
+            throw new InvalidOperationException("Попытка добавления значения после узла, не принадлежащего текущему списку");
+
+        if (ReferenceEquals(Position, First))
+            return AddFirst(value);
+
+        var node = new Node(value, this)
+        {
+            Next = Position,
+            Prev = Position.Prev,
+        };
+
+        Position.Prev = node;
+        node.Prev!.Next = node;
+
+        return node;
+    }
+
+    public T Remove(Node node)
+    {
+        if (!ReferenceEquals(this, node._List))
+            throw new InvalidOperationException("Попытка удаление узла, не принадлежащего текущему списку");
+
+        node._List = null;
+
+        if (_Count == 1)
+        {
+            First = null;
+            Last = null;
+        }
+        else if (ReferenceEquals(First, node))
+        {
+            First = node.Next;
+            First.Prev = null;
+        }
+        else if (ReferenceEquals(Last, node))
+        {
+            Last = node.Prev;
+            Last.Next = null;
+        }
+        else
+        {
+            node.Prev.Next = node.Next;
+            node.Next.Prev = node.Prev;
+        }
+
+        node.Next = null;
+        node.Prev = null;
+
+        _Count--;
+
+        return node.Value;
+    }
+
+    public void Clear() // O(n)
+    {
+        if (_Count == 0) return;
+
+        var node = First;
+        while (node != null)
+        {
+            node._List = null;
+            node.Prev = null;
+            var tmp = node.Next;
+            node.Next = null;
+
+            node = tmp;
+        }
+
+        First = null;
+        Last = null;
+        _Count = 0;
     }
 }
