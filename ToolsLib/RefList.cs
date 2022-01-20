77e48eb4910e8ef5345b6e94a3ffe28b8b7bf609
Infarh <shmachilin@yandex.ru>
2022-01-20 19:32:12 +0300

    Заготовка реализации двусвязного списка

---
+++ ToolsLib/RefList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolsLib;
+
+public class RefList<T>
+{
+    public RefList()
+    {
+        
+    }
+}
