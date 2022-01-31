7c70856903e161d98a8db703d97be89de3f5e22f
Infarh <shmachilin@yandex.ru>
2022-01-31 20:42:50 +0300

    Реализация хранилищ с помощью абстрактного класса

--- StudentsOperations/Storages/LectorsStorage.cs
+++ StudentsOperations/Storages/LectorsStorage.cs
@@ -1,47 +1,11 @@
 ﻿using StudentsOperations;
+using StudentsOperations.Storages.Base;
 
 namespace LectorsOperations.Storages;
 
-public class LectorsStorage
+public class LectorsStorage : Storage<Lector>
 {
-    private int _LastFreeId = 1;
-    private List<Lector> _Lectors = new();
-
-    public IEnumerable<Lector> GetAll()
-    {
-        return _Lectors.AsReadOnly();
-    }
-
-    public Lector? GetById(int Id)
-    {
-        //foreach(var lector in _Lectors)
-        //    if (lector.Id == Id)
-        //        return lector;
-
-        //return null;
-
-        return _Lectors.FirstOrDefault(lector => lector.Id == Id);
-    }
-
-    /// <summary>Добавление нового лектора в хранилище</summary>
-    /// <param name="Lector">Добавляемый лектор</param>
-    /// <returns>Возвращает значение назначенного идентификатора</returns>
-    /// <exception cref="ArgumentNullException">Если передана пустая ссылка <paramref name="Lector"/></exception>
-    public int Add(Lector Lector)
-    {
-        if(Lector is null) throw new ArgumentNullException(nameof(Lector));
-
-        if (_Lectors.Contains(Lector)) // только для данной реализации!!! Когда в БД будет - это писать НЕ НАДО!!!
-            return Lector.Id;
-
-        Lector.Id = _LastFreeId; 
-        _LastFreeId++;
-        _Lectors.Add(Lector);
-
-        return Lector.Id;
-    }
-
-    public bool Edit(Lector Lector)
+    public override bool Edit(Lector Lector)
     {
         if (Lector is null) throw new ArgumentNullException(nameof(Lector));
 
@@ -61,16 +25,5 @@ public class LectorsStorage
         // SaveChanges();
 
         return true;
-    }
-
-    public Lector? Remove(int Id)
-    {
-        var db_lector = GetById(Id);
-        if (db_lector is null)
-            return null;
-
-        _Lectors.Remove(db_lector);
-
-        return db_lector;
     }
 }
--- StudentsOperations/Storages/StudentsStorage.cs
+++ StudentsOperations/Storages/StudentsStorage.cs
@@ -1,45 +1,10 @@
-﻿namespace StudentsOperations.Storages;
+﻿using StudentsOperations.Storages.Base;
 
-public class StudentsStorage
+namespace StudentsOperations.Storages;
+
+public class StudentsStorage : Storage<Student>
 {
-    private int _LastFreeId = 1;
-    private List<Student> _Students = new();
-
-    public IEnumerable<Student> GetAll()
-    {
-        return _Students.AsReadOnly();
-    }
-
-    public Student? GetById(int Id)
-    {
-        //foreach(var student in _Students)
-        //    if (student.Id == Id)
-        //        return student;
-
-        //return null;
-
-        return _Students.FirstOrDefault(student => student.Id == Id);
-    }
-
-    /// <summary>Добавление нового студента в хранилище</summary>
-    /// <param name="Student">Добавляемый студент</param>
-    /// <returns>Возвращает значение назначенного идентификатора</returns>
-    /// <exception cref="ArgumentNullException">Если передана пустая ссылка <paramref name="Student"/></exception>
-    public int Add(Student Student)
-    {
-        if(Student is null) throw new ArgumentNullException(nameof(Student));
-
-        if (_Students.Contains(Student)) // только для данной реализации!!! Когда в БД будет - это писать НЕ НАДО!!!
-            return Student.Id;
-
-        Student.Id = _LastFreeId; 
-        _LastFreeId++;
-        _Students.Add(Student);
-
-        return Student.Id;
-    }
-
-    public bool Edit(Student Student)
+    public override bool Edit(Student Student)
     {
         if (Student is null) throw new ArgumentNullException(nameof(Student));
 
@@ -59,16 +24,5 @@ public class StudentsStorage
         // SaveChanges();
 
         return true;
-    }
-
-    public Student? Remove(int Id)
-    {
-        var db_student = GetById(Id);
-        if (db_student is null)
-            return null;
-
-        _Students.Remove(db_student);
-
-        return db_student;
     }
 }
