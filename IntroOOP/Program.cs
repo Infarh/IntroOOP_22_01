96354136ee078aed9e104577ffb8b1bca6e538e0
Infarh <shmachilin@yandex.ru>
2022-01-31 20:50:02 +0300

    Удаление хлама

--- IntroOOP/Program.cs
+++ IntroOOP/Program.cs
@@ -34,6 +34,7 @@ public static class Program
         Print(course);
         Print(group);
 
+        var random = new Random(5);
         var students = Enumerable.Range(1, 100).Select(
             i => new Student
             {
@@ -41,46 +42,12 @@ public static class Program
                 LastName = $"Last name - {i}",
                 FirstName = $"First name - {i}",
                 Patronymic = $"Patronymic - {i}",
+                Rating = random.NextDouble() * 100,
             }).ToArray();
 
         foreach (var student in students)
             __StudentsStorage.Add(student);
 
-        var students_first_name_dict = students.ToDictionary(s => s.LastName, s => s.FirstName);
-
-        var test_student = students_first_name_dict["Last name - 53"];
-        //var test_student = students.First(stud => stud.LastName == "Last name - 53");
-
-        var decanat = new Decanat();
-
-        var group1 = new StudentsGroup { Name = "Группа-1" };
-
-        decanat.AddStudent(new Student
-        {
-            LastName = "Иванов",
-            FirstName = "Иван",
-            Patronymic = "Иванович",
-        }, group1);
-
-        decanat.AddStudent(new Student
-        {
-            LastName = "Петров",
-            FirstName = "Пётр",
-            Patronymic = "Петрович",
-        }, group1);
-
-        decanat.AddStudent(new Student
-        {
-            LastName = "Сидоров",
-            FirstName = "Сидор",
-            Patronymic = "Сидорович",
-        }, new StudentsGroup { Name = "Группа-2" });
-
-
-        var students_ratings = new Dictionary<Student, double>();
-
-        var students_set = new HashSet<Student>();
-
-        Console.ReadLine();
+        
     }
 }
