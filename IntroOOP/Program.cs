39ab0379e67f2f595ab27f5642ca00fd70562fe7
Infarh <shmachilin@yandex.ru>
2022-02-03 20:55:20 +0300

    Командный процессор

--- IntroOOP/Program.cs
+++ IntroOOP/Program.cs
@@ -4,6 +4,31 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-        FileOperations.Test();
+        //FileOperations.Test();
+
+        var do_work = true;
+        while (do_work)
+        {
+            Console.Write("Введите команду >");
+            var command = Console.ReadLine();
+
+            switch (command.ToLower())
+            {
+                case "copy": break;
+                case "cd": break;
+                case "md": break;
+                case "help":
+                    Console.WriteLine("copy, cd, md, help");
+                    break;
+                case "exit":
+                    do_work = false;
+                    break;
+                default:
+                    Console.WriteLine("Непонятная команда {0}", command);
+                    break;
+            }
+        }
+
+        Console.WriteLine("Программа завершена");
     }
 }
